9c3457afbd1567ed9f1b7d4008befe4e90079011
Syahrial Rafky <93416709+SkyLumi@users.noreply.github.com>
2025-12-08 06:34:45 +0700

    tambah dialog

---
+++ src/Dialog/Scripts/stage_1_timeline_1.gd
@@ -0,0 +1,15 @@
+extends Area3D
+
+@export var dialog_path : String = "res://src/Dialog/stage1-timeline1.dtl"
+
+func _ready():
+	connect("body_entered", _on_body_entered)
+
+func _on_body_entered(body):
+	if body.name == "player":
+		print("player masuk")
+		call_deferred("_start_dialog")
+
+func _start_dialog():
+	var dialog = Dialogic.start(dialog_path)
+	get_tree().current_scene.add_child(dialog)
